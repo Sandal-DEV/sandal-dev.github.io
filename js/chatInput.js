fbf489ae3b8cba4da4850dcc2f9248226ec1d506
Daniel Ku <reby7146@me.com>
2021-04-18 00:51:23 +0900

    wafwaf

--- js/chatInput.js
+++ js/chatInput.js
@@ -1,39 +1,39 @@
 // const sendBtn = document.querySelector('.chat-send-btn'),
 // const chatText = document.querySelector('.chat-textarea');
 
-function activeBtn() {
-    const sendBtn = document.querySelector('.chat-send-btn');
-    const chatText = document.querySelector('.chat-textarea');
-    if (chatText.value !== "") {
-        sendBtn.id = "activate";
-        sendBtn.disabled = false;
-    } else if (chatText.value == "") {
-        sendBtn.id = "";
-        sendBtn.disabled = true;
-    }
-}
+// function activeBtn() {
+//     const sendBtn = document.querySelector('.chat-send-btn');
+//     const chatText = document.querySelector('.chat-textarea');
+//     if (chatText.value !== "") {
+//         sendBtn.id = "activate";
+//         sendBtn.disabled = false;
+//     } else if (chatText.value == "") {
+//         sendBtn.id = "";
+//         sendBtn.disabled = true;
+//     }
+// }
 
-function enterHandler(event) {
-    const keyCode = event.keyCode;
-    const sendBtn = document.querySelector('.chat-send-btn');
-    const chatText = document.querySelector('.chat-textarea');
+// function enterHandler(event) {
+//     const keyCode = event.keyCode;
+//     const sendBtn = document.querySelector('.chat-send-btn');
+//     const chatText = document.querySelector('.chat-textarea');
 
-    if (keyCode == 13) {
-        if (!event.shiftKey) {
-            event.preventDefault();
-            DotNet.invokeMethodAsync("Sandal.WASM", 'OnSend');//.then(data => data);
-            chatText.value = "";
-        } else {
-            chatText.value += "\r\n";
-        }
-    }
-}
+//     if (keyCode == 13) {
+//         if (!event.shiftKey) {
+//             event.preventDefault();
+//             DotNet.invokeMethodAsync("Sandal.WASM", 'OnSend');//.then(data => data);
+//             chatText.value = "";
+//         } else {
+//             chatText.value += "\r\n";
+//         }
+//     }
+// }
 
-function getChatValue() {
-    const chatText = document.querySelector('.chat-textarea');
-    return chatText.value;
-    //chatText.value = "";
-}
+// function getChatValue() {
+//     const chatText = document.querySelector('.chat-textarea');
+//     return chatText.value;
+//     //chatText.value = "";
+// }
 
 // 전송 버튼 클릭 시 실행되는 action
 // blazor로 메세지 전송
